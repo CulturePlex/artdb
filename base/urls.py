81464252c6b3f7e769558e3a3d50937ce3dfec74
natalia <n.mejiacaldas@gmail.com>
2013-11-18 19:20:08 -0500

    Changed urls that contain underscore to containing a hyphen.

--- base/urls.py
+++ base/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('base.views',
     url(r'^$', 'public_view', name="public_view"),
 
-    url(r'advanced_search/$', 'advanced_search', name="advanced_search"),
-    url(r'search_results/$', 'search_results', name="search_results"),
+    url(r'advanced-search/$', 'advanced_search', name="advanced_search"),
+    url(r'search-results/$', 'search_results', name="search_results"),
 
     url(r'contact/$', 'contact', name="contact"),
     url(r'about/$', 'about', name="about"),
-    url(r'cite_us/$', 'cite_us', name="cite_us"),
+    url(r'cite-us/$', 'cite_us', name="cite_us"),
     url(r'login/$', 'login', name="login")
 )
