beb9422c35638a96739b043a493b46d1d8e98864
natalia <n.mejiacaldas@gmail.com>
2014-01-28 13:20:39 -0500

    No changes were made.

--- base/utils.py
+++ base/utils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 def url_map(lat, lon, width=300, height=300, marker_size='tiny', map_type="roadmap"):
-    return u"http://maps.google.com/staticmap?center=%s,%s&zoom=15&size=%sx%s&maptype=%s&key=%s&markers=%s,%s,%sblue" % (str(lat), str(lon), str(width), str(height), map_type, settings.GOOGLE_API_KEY, str(lat), str(lon), marker_size)
+    return u"http://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=15&size=%sx%s&maptype=%s&key=%s&markers=%s,%s,%sblue" % (str(lat), str(lon), str(width), str(height), map_type, settings.GOOGLE_API_KEY, str(lat), str(lon), marker_size)
 
 
 def url_map_points(points, width=300, height=300, create_path=True, marker_size='tiny', map_type="roadmap"):
