4785d611fe841b08abdcb121e3a8979b56730f87
Javier de la Rosa <versae@gmail.com>
2014-02-11 15:26:44 -0500

    Update static maps URL

--- base/utils.py
+++ base/utils.py
@@ -6,11 +6,11 @@ def url_map(lat, lon, width=300, height=300, marker_size='tiny', map_type="roadm
     return u"http://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=15&size=%sx%s&maptype=%s&key=%s&markers=%s,%s,%sblue" % (str(lat), str(lon), str(width), str(height), map_type, settings.GOOGLE_API_KEY, str(lat), str(lon), marker_size)
 
 
-def url_map_points(points, width=300, height=300, create_path=True, marker_size='tiny', map_type="roadmap"):
+def url_map_points(points, width=300, height=300, create_path=True, marker_size='tiny', map_type="terrain"):
     if len(points) == 1:
         lat = points[0][1]
         lon = points[0][0]
-        return u"http://maps.google.com/staticmap?center=%s,%s&zoom=5&size=%sx%s&maptype=%s&key=%s&markers=%s,%s,%sblue" % (str(lat), str(lon), str(width), str(height), map_type, settings.GOOGLE_API_KEY, str(lat), str(lon), marker_size)
+        return u"http://maps.googleapis.com/maps/api/staticmap?sensor=false&center=%s,%s&zoom=5&size=%sx%s&maptype=%s&key=%s&markers=size:%s|color:blue|%s,%s" % (str(lat), str(lon), str(width), str(height), map_type, settings.GOOGLE_API_KEY, marker_size, str(lat), str(lon))
     else:
         if create_path:
             path = "|".join(["%s,%s" % (lat, lon) for lat, lon in points])
