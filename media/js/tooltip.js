990b51c5771eab78379cace898c7fea6f22a59fe
Javier de la Rosa <versae@gmail.com>
2014-02-11 15:01:03 -0500

    Add a preview for map locations

--- media/js/tooltip.js
+++ media/js/tooltip.js
@@ -1,47 +1,49 @@
 /*
- * Image preview script 
+ * Image preview script
  * powered by jQuery (http://www.jquery.com)
- * 
+ *
  * written by Alen Grakalic (http://cssglobe.com)
- * 
+ *
  * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
  *
  */
- 
-this.imagePreview = function(){	
-	/* CONFIG */
-		
-		xOffset = 10;
-		yOffset = 30;
-		
-		// these 2 variable determine popup's distance from the cursor
-		// you might want to adjust to get the right result
-		
-	/* END CONFIG */
-	$("a.preview").click(function(e){
 
-		
-		if($("#preview").length > 0){
-			$("#preview").remove();}
-else
-{
-		this.t = this.title;
-		this.title = "";	
-		var c = (this.t != "") ? "<br/>" + this.t : "";
-		$("body").append("<p id='preview'><a href='"+ this.rel +"'><img src='"+ this.href +"' alt='Image preview' /></a>"+ c +"</p>");								 
-		$("#preview")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px")
-			.fadeIn("fast");}
-	return false;						
-    },
-	function(){
-		this.title = this.t;	
-		$("#preview").remove();
-    });			
+this.imagePreview = function() {
+    /* CONFIG */
+
+    xOffset = 10;
+    yOffset = 30;
+
+    // these 2 variable determine popup's distance from the cursor
+    // you might want to adjust to get the right result
+
+    /* END CONFIG */
+    $("a.preview").hover(function(e) {
+            this.t = this.title;
+            this.title = "";
+            var c = (this.t != "") ? "<br/>" + this.t : "";
+            $("body").append("<p id='preview'><img src='" + this.href + "' alt='Image preview' />" + c + "</p>");
+            $("#preview")
+                .css("top", (e.pageY - xOffset) + "px")
+                .css("left", (e.pageX + yOffset) + "px")
+                .fadeIn("fast");
+        },
+        function() {
+            this.title = this.t;
+            $("#preview").remove();
+        });
+    $("a.preview").mousemove(function(e) {
+        $("#preview")
+            .css("top", (e.pageY - xOffset) + "px")
+            .css("left", (e.pageX + yOffset) + "px");
+    });
+    $("a.preview").click(function(e) {
+    	e.preventDefault();
+    });
 };
 
+
 // starting the script on page load
-$(document).ready(function(){
-	imagePreview();
+$(document).ready(function() {
+    imagePreview();
 });
